73b0397f73ae398a70cd3705736803af8555f682
ranjay17 <sranjay15@gmail.com>
2024-07-27 01:31:03 +0530

    Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl bg-green-600">
-      Hello World
-    </div>
+    <Body />
   );
 }
 
