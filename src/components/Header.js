de0f9158e5bd627f7931c9c10e0310e202fddb97
Ranjay Singh <Sranjay15@gmail.com>
2024-08-12 15:44:01 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,14 +55,14 @@ const Header = () => {
 return () => unsubscribe()
   },[dispatch, navigate]);
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img
-      className="w-44"
+      className="w-44 mx-auto md:mx-0"
       src={logo}
       alt="logo"
       />
       {user && (
-        <div className="flex p-2">
+        <div className="flex justify-between p-2">
           {showGptSearch &&(
           <select 
           className='p-2 m-2 bg-gray-900 text-white' 
@@ -78,7 +78,7 @@ return () => unsubscribe()
             {showGptSearch ? "Home" : "GPT Search"}
           </button>
         <img
-        className="w-12 h-12 mx-2 my-2"
+        className="hidden md:block w-12 h-12 mx-2 my-2"
         src= {user.photoURL}
         alt="userIcon"
         />
