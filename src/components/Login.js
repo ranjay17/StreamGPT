f43a3df9db2164054f680ab0f9c9c794a6648e92
Ranjay Singh <Sranjay15@gmail.com>
2024-08-12 15:46:00 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -69,11 +69,11 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img src={BACKGROUND_IMG} alt="" />
+        <img className="h-screen object-cover md:h-full" src={BACKGROUND_IMG} alt="" />
       </div>
       <form 
         onSubmit={(e) => e.preventDefault()}
-        className="w-3/12 absolute p-12 bg-black my-36 mx-auto left-0 right-0 text-white rounded-lg bg-opacity-80">
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto left-0 right-0 text-white rounded-lg bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
